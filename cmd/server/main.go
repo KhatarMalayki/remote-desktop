a5f3041d0d157d8a64a415339b299a0bdf771da2
Carisa Admin <admin@carisa.id>
2026-09-21 11:06:46 +0700

    fix(release): report server version 0.2.21

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/user/remote-desktop/internal/server"
 )
 
-var version = "0.2.20"
+var version = "0.2.21"
 
 func main() {
 	addr := flag.String("addr", ":8080", "listen address")
